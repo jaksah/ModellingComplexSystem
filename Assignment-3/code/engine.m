a28cc04b795e2093d6f5c08c28f5625cebe8a1ba
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2014-06-04 14:00:45 +0200

    Direction calculations

---
+++ Assignment-3/code/engine.m
@@ -0,0 +1,8 @@
+
+N = 10; % Number of individuals
+T = 100; % Number of timesteps
+w = 1; % Weight of influence of preferred direction
+p = 0.1; % Proportion of indiviuals with knowledge of preferred direction
+a = 1; % Minimum distance between particles
+plotornot = 1; 
+couzin(N, T, w, p, plotornot);
