9f2512e87f5acb68b41c7d2bc4a4fea453081108
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2014-06-14 17:54:12 +0200

    Master equation

--- Assignment-3/code/engine.m
+++ Assignment-3/code/engine.m
@@ -1,6 +1,6 @@
 
 %N = 30; % Number of individuals
-T = 500; % Number of timesteps
+T = 1500; % Number of timesteps
 w = 0.5; % Weight of influence of preferred direction
 %p = 0.1; % Proportion of indiviuals with knowledge of preferred direction
 a = 1; % Minimum distance between particles
@@ -8,7 +8,7 @@ rho = 6; % Radius of particles to interact with
 gam = 0; % Uncertainty of preferred direction
 plotornot = 0; 
 reps = 10;
-g = [1;1];
+g = [1;0];
 gAngle = atan2(g(2),g(1));
 
 %Nvals = [10 30 50 100 200];
@@ -29,17 +29,23 @@ for Nidx=1:length(Nvals)
 			toc
 			[e, cent] = couzin(N, T, w, p, a, rho, gam, g, plotornot);
 			vec = cent(:,end) - cent(:,end-50);
-			angles(rep) = atan2(vec(2),vec(1));
+			angles(rep) = abs(atan2(vec(2),vec(1)))/pi;
 			elongvals(pidx,Nidx) = elongvals(pidx,Nidx) + mean(e(end-100:end));
 		end
-		accuracy(pidx, Nidx) = mean(sum((angles-gAngle).^2));
+		accuracy(pidx, Nidx) = mean((angles-gAngle).^2);
 		elongvals(pidx,Nidx) = elongvals(pidx,Nidx)/reps;
         groupdirvals(pidx,Nidx) = groupdirvals(pidx,Nidx)/reps;
 	end
 end
+
+%% 
+pvals = 0:0.1:1;
+plot(pvals,accuracy(:,1),'o-')
+xlabel('p')
+ylabel('Accuracy')
+hold on
 %%
 
-load dataN50
 plot(pvals,elongvals(:,1),'o-')
 xlabel('p')
 ylabel('Elongitude')
@@ -52,8 +58,26 @@ plot(pvals, y,'.')
 set(gca, 'YLim', [0 10])
 legend('Simulation', 'Approximation')
 
-%% 
-plot(pvals,accuracy(:,1),'o-')
-xlabel('p')
-ylabel('Accuracy')
+%%
+alpha = 1;
+myMax = 5;
+x1 = 1:alpha:myMax;
+x2 = x1-alpha/2;
+y1 = x1;
+y2 = x1+alpha;
+figure;
+xUn = [x1(1:end-2) x2(1:end-2)];
+xIn = [x1(end-1:end) x2(end-1:end)];
+yUn = [y1(1:end-2) y2(1:end-2)];
+yIn = [y1(end-1:end) y2(end-1:end)];
+plot(xUn,yUn,'k.')
 hold on
+plot(xIn,yIn,'ko')
+
+set(gca, 'YLim', [-1 myMax+2], 'XLim', [-1 myMax+1]);
+legend('Uninformed','Informed')
+
+
+
+
+
