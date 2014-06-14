e39607ab6859f97d47bed7c205f5d560de2a0116
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2014-06-14 16:23:08 +0200

    Accuracy plot

--- Assignment-3/code/engine.m
+++ Assignment-3/code/engine.m
@@ -8,29 +8,48 @@ rho = 6; % Radius of particles to interact with
 gam = 0; % Uncertainty of preferred direction
 plotornot = 0; 
 reps = 1;
+g = [1;1];
+gAngle = atan2(g(2),g(1));
 
 %Nvals = [10 30 50 100 200];
 Nvals = [30];
 pvals = 0:0.05:1;
 elongvals = zeros(length(pvals),length(Nvals));
+accuracy = zeros(length(pvals),length(Nvals));
 tic;
 for Nidx=1:length(Nvals)
 	N = Nvals(Nidx)
 	for pidx=1:length(pvals)
 		p = pvals(pidx)
+		angles = zeros(1,reps);
 		for rep=1:reps
 			rep
 			toc
-			[e, groupdir] = couzin(N, T, w, p, a, rho, gam, plotornot);
+			[e, cent] = couzin(N, T, w, p, a, rho, gam, g, plotornot);
+			vec = cent(:,end) - cent(:,end-50);
+			angles(rep) = atan2(vec(2),vec(1));
 			elongvals(pidx,Nidx) = elongvals(pidx,Nidx) + mean(e(end-100:end));
 		end
+		accuracy(pidx, Nidx) = mean(sum((angles-gangle).^2));
 		elongvals(pidx,Nidx) = elongvals(pidx,Nidx)/reps;
 	end
 end
 %%
+load dataN50
 plot(pvals,elongvals(:,1),'o-')
 xlabel('p')
 ylabel('Elongitude')
 hold on
-y = 1./pvals;
+pvals = 0:0.01:1;
+
+y = (N./(((sqrt(N.*pvals)-ones(size(pvals)))*a).^2));
+%y = (2./pvals);
 plot(pvals, y,'.')
+set(gca, 'YLim', [0 10])
+legend('Simulation', 'Approximation')
+
+%% 
+plot(pvals,accuracy,'o-')
+xlabel('p')
+ylabel('Accuracy')
+hold on
